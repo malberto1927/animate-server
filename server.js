961315119a6b7bf69aa546e68039e7033eb5408f
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-18 00:44:17 -0500

    Se genero un metodo asyncrono para leer el archivo, tambien se manejaron direcciones de dichos archivos que sean compatibles en cualquier SO

--- server.js
+++ server.js
@@ -2,20 +2,26 @@
 
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 const port = process.env.PORT || 8080
 
 // los callback son funciones que se ejecutan 
-// despues de una operacion asincrona
+// despues de una operacion asyncrona
 const server = http.createServer()
 
 server.on('request', onRequest)
 server.on('listening', onListening)
 
 server.listen(port)
-
+//un metodo asyncrono simpre lleva un callback
 function onRequest(req, res){
-	let file = fs.readFileSync('public/index.html')
-	res.end(file)
+	let fileName = path.join(__dirname, 'public', 'index.html')
+	fs.readFile(fileName,function(err, file){
+		if(err){
+			return res.end(err.message)
+		}
+		res.end(file)
+	})
 }
 
 function onListening(){
