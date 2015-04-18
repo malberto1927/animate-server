30c0fe7acecdb84baeb5577efecdf2d753ab9fa0
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-18 14:47:30 -0500

    Se empezo a trabajar con el modulo st para node el cual nos permite hacer llamadas a archivos staticos en nuestra aplicacion

--- server.js
+++ server.js
@@ -1,61 +1,17 @@
 'use strict'
 
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const router = require('./router')
 const port = process.env.PORT || 8080
 
 // los callback son funciones que se ejecutan 
 // despues de una operacion asyncrona
 const server = http.createServer()
 
-server.on('request', onRequest)
+server.on('request', router)
 server.on('listening', onListening)
 
 server.listen(port)
-//un metodo asyncrono simpre lleva un callback
-function onRequest(req, res){
-	let url = req.url
-	
-	if(url.startsWith('/index') || url === '/'){
-		return serveIndex(res)
-	}
-
-	if(url === '/app.js'){
-		return serveApp(res)
-	}
-
-	res.statusCode = 404
-	res.end(`404 not found: ${url}`)
-}
-
-function serveIndex(res){
-	let index = path.join(__dirname, 'public', 'index.html')
-	
-	res.setHeader('Content-Type','text/html')
-	let rs = fs.createReadStream(index)
-
-	rs.pipe(res)
-
-	rs.on('error', function(err){
-		res.setHeader('Content-Type','text/plain')
-		res.end(err.message)
-	})
-}
-
-function serveApp(res){
-	let app = path.join(__dirname, 'public', 'app.js')
-	
-	res.setHeader('Content-Type','text/javascript')
-	let rs = fs.createReadStream(app)
-
-	rs.pipe(res)
-
-	rs.on('error', function(err){
-		res.setHeader('Content-Type','text/plain')
-		res.end(err.message)
-	})
-}
 
 function onListening(){
 	console.log(`Servidor escuchando en el puerto ${port}`)
