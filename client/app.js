0f04a0cc0d614e4b527f5115643c9e6bc1e175c8
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-18 15:35:06 -0500

    Se instalo browserify y webrtc2images para poder grabar desde la webcam

---
+++ client/app.js
@@ -0,0 +1,25 @@
+const Webrtc2images = require('webrtc2images')
+
+const rtc = new Webrtc2images({
+	width: 200,
+	height: 200,
+	frames: 10,
+	type: "image/jpeg",
+	quality: 0.4,
+	interval: 200
+})
+
+rtc.startVideo(function(err){
+
+})
+
+const record = document.querySelector('#record')
+
+record.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  rtc.recordVideo(function(err, frames){
+  	console.log(frames)
+  })
+
+}, false)
