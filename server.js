0e5cc15a90ecf52012a521b9064e9f6302f3f78a
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-17 23:36:43 -0500

    Se crearon callbacks para que se genera una respuesta del servidor

--- server.js
+++ server.js
@@ -3,6 +3,12 @@
 const http = require('http')
 const port = process.env.PORT || 8080
 
-const server = http.createServer()
+// los callback son funciones que se ejecutan 
+// despues de una operacion asincrona
+const server = http.createServer(function(req, res){
+	res.end('Hola io.js')
+})
 
-server.listen(port)
+server.listen(port,function(){
+	console.log('Servidor escuchando en el puerto ' + port)
+})
