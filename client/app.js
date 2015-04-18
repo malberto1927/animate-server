93e59b76d1b0284fcd7a69363a056915a566b57f
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-18 16:29:02 -0500

    Se instalo xhr que nos ayuda para la implementacion de ajax y body para el manejo de json del usuario al servidor

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+const xhr = require('xhr')
 const Webrtc2images = require('webrtc2images')
 
 const rtc = new Webrtc2images({
@@ -9,8 +10,8 @@ const rtc = new Webrtc2images({
 	interval: 200
 })
 
-rtc.startVideo(function(err){
-
+rtc.startVideo(function (err){
+	if(err) return logError(err)
 })
 
 const record = document.querySelector('#record')
@@ -18,8 +19,23 @@ const record = document.querySelector('#record')
 record.addEventListener('click', function (e) {
   e.preventDefault()
 
-  rtc.recordVideo(function(err, frames){
-  	console.log(frames)
+  rtc.recordVideo(function (err, frames){
+  	if(err) return logError(err)
+
+  	xhr({
+  		url: '/process',
+  		method: 'post',
+  		headers: { 'Content-type': 'application/json' },
+  		body: JSON.stringify({ images:frames }),
+  	}, function (err, res, body){
+  		if(err) return logError(err)
+
+  		console.log(JSON.parse(body))
+  	})
   })
 
 }, false)
+
+function logError (err) {
+  console.error(err)
+}
