93f1556788949c0209796b7fc5c5ae390ece03f4
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-18 14:00:07 -0500

    Se modifico el servidor para que pudiera mostrar archivos segun la url solicitada en el navegador

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ server.on('listening', onListening)
 server.listen(port)
 //un metodo asyncrono simpre lleva un callback
 function onRequest(req, res){
+	let url = req.url
+	
+	if(url.startsWith('/index') || url === '/'){
+		return serveIndex(res)
+	}
+
+	if(url === '/app.js'){
+		return serveApp(res)
+	}
+
+	res.statusCode = 404
+	res.end(`404 not found: ${url}`)
+}
+
+function serveIndex(res){
 	let index = path.join(__dirname, 'public', 'index.html')
 	
 	res.setHeader('Content-Type','text/html')
@@ -23,6 +38,21 @@ function onRequest(req, res){
 	rs.pipe(res)
 
 	rs.on('error', function(err){
+		res.setHeader('Content-Type','text/plain')
+		res.end(err.message)
+	})
+}
+
+function serveApp(res){
+	let app = path.join(__dirname, 'public', 'app.js')
+	
+	res.setHeader('Content-Type','text/javascript')
+	let rs = fs.createReadStream(app)
+
+	rs.pipe(res)
+
+	rs.on('error', function(err){
+		res.setHeader('Content-Type','text/plain')
 		res.end(err.message)
 	})
 }
