204ed0ec83313a2121b130aa13d74d21f07dfff3
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-17 23:59:59 -0500

    Se genero un codigo mas limpio y mas mantenible

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const port = process.env.PORT || 8080
 
 // los callback son funciones que se ejecutan 
 // despues de una operacion asincrona
-const server = http.createServer(function(req, res){
-	res.end('Hola io.js')
-})
+const server = http.createServer(onRequest)
 
-server.listen(port,function(){
+server.listen(port, onListening)
+
+function onRequest(req, res){
+	res.end('Hola io.js')
+}
+
+function onListening(){
 	console.log('Servidor escuchando en el puerto ' + port)
-})
+}
