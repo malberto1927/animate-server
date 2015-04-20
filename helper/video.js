01e4101e804a992df684c1252f5fccd42269e0bf
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-04-20 12:19:49 -0500

    Se utilizo async para lanzar en secuencia las funciones necesarias para procesamiento de las imagenes

--- helper/video.js
+++ helper/video.js
@@ -1,13 +1,39 @@
 'use strict'
 
+const async = require('async')
 const EventEmitter = require('events').EventEmitter
 
 module.exports = function (images) {
   let events = new EventEmitter()
 
-  setTimeout(function () {
-    events.emit('video', 'this will be the encoded video')
-  }, 1000)
+  async.series([
+  	decodeImages,
+  	createVideo,
+  	encodeVideo,
+  	cleanup
+  	],convertFinished)
+
+  function decodeImages(done){
+  	done()
+  }
+
+  function createVideo(done){
+  	done()
+  }
+
+  function encodeVideo(done){
+  	done()
+  }
+
+  function cleanup(done){
+  	done()
+  }
+
+  function convertFinished(err){
+  	setTimeout(function () {
+	    events.emit('video', 'this will be the encoded video')
+	  }, 1000)
+  }
 
   return events
 }
